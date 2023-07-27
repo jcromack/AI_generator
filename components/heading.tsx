3acc5273e7ccc76c1491ad0b455aec155f62be0a
jcromack <cromackjames@gmail.com>
2023-07-27 17:15:05 +0100

    yes

---
+++ components/heading.tsx
@@ -0,0 +1,23 @@
+import { LucideIcon } from "lucide-react";
+
+interface HeadingProps {
+    title: string;
+    description: string;
+    icon: LucideIcon;
+    iconColor?: string;
+    bgColor?: string;
+}
+
+export const Heading = ({
+    title,
+    description,
+    icon,
+    iconColor,
+    bgColor
+}: HeadingProps ) => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
